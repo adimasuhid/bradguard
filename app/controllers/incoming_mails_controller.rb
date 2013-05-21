dd605685aced5afa881ef37156e1958cb275bac5
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2013-05-21 12:04:53 +0800

    Adds Send Texts

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,16 +1,19 @@
 class IncomingMailsController < ApplicationController
   def create
-    Rails.logger.info params[:plain]
-    puts params[:plain]
-    puts params[:envelope][:from]
 
     if params[:envelope][:from].present?
       @message = IncomingMail.create!(sender: params[:envelope][:from], subject: params[:headers][:Subject], message: params[:plain])
+      @sms_message = "From:#{@message.sender} Subject:#{@message.subject}"
       puts "Yey in here"
+
+      IncomingMail.send_sms("09172416140",@sms_message) #Ace
+      IncomingMail.send_sms("09064774979",@sms_message) #Ken
+
       render :text => 'success', :status => 200
     else
       puts "Oh no unknown"
       render :text => 'Unknown user', :status => 404
     end
   end
+
 end
--- app/models/incoming_mail.rb
+++ app/models/incoming_mail.rb
@@ -1,3 +1,11 @@
 class IncomingMail < ActiveRecord::Base
   attr_accessible :message, :sender, :subject
+
+  API_HOST = "https://fireflyapi.com/api/sms"
+  API_KEY = "9uzQsvMNTyggsNvkirPN"
+
+  def self.send_sms(number,message)
+    response = RestClient.post API_HOST, { api: API_KEY, number: number, message: message}
+    puts response
+  end
 end
