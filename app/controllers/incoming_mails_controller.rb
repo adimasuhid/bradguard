3224b77d9c38c89a2a347320da504363c84814c3
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2013-06-01 22:25:44 +0800

    Refactor

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,27 +1,6 @@
 class IncomingMailsController < ApplicationController
   include Mandrill::Rails::WebHookProcessor
 
-  def index
-    render :text => 'success', :status => 200
-  end
-
-  #def create
-    #if params[:envelope][:from].present?
-      #@mail = IncomingMail.create!(sender: params[:envelope][:from], forwarder: params[:envelope][:to], subject: params[:headers][:Subject], message: params[:plain])
-      #puts "Yey in here"
-
-      #@recipients = RecipientList.where(mail_sender: @mail.forwarder).first.recipients
-      #@recipients.each do |recipient|
-        #IncomingMail.send_sms(recipient.number,@mail.short_message) 
-      #end
-
-      #render :text => 'success', :status => 200
-    #else
-      #puts "Oh no unknown"
-      #render :text => 'Unknown user', :status => 404
-    #end
-  #end
-
   def handle_inbound(event_payload)
     if event_payload["msg"]["from_email"].present?
       @attrs = {
@@ -42,15 +21,9 @@ class IncomingMailsController < ApplicationController
         end
       end
 
-      render :text => 'success', :status => 200
     else
       puts "Oh no unknown"
-      render :text => 'Unknown user', :status => 404
     end
-  end
-
-  def show
-    render :text => 'success', :status => 200
   end
 
 end
